b1a17d44da2dd36f94fb2d4f30eeca458873ead3
RoieGolst <roiegol9@gmail.com>
2024-12-08 10:20:02 +0200

    Fix the runner script

--- scripts/run_autoRSA.sh
+++ scripts/run_autoRSA.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 WORK_DIR="$(dirname "$0")/.."
 
-source "$WORK_DIR/.venv/bin/activate"
-python3.12 "$WORK_DIR/autoRSA.py" "$@"
+cd "$WORK_DIR" || echo "Failed moving to working dir"
+source ./.venv/bin/activate
+./.venv/bin/python3.12 ./autoRSA.py "$@"
